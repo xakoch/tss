d7f79dab332d2b6708df636e63d71621605c61d3
Timur Turabaev <xakoch13@gmail.com>
2025-03-13 19:14:35 +0500

    update index.html

---
+++ dist/assets/js/app.js
@@ -0,0 +1,5 @@
+/**************************************************************
+* Lazy-load
+**************************************************************/
+const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+observer.observe();
